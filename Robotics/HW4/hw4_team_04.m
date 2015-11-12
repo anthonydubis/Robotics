2a34aa3639625e7f49add2bddc6689dc2867f425
Anthony Dubis <anthonydubis@gmail.com>
2015-11-12 12:49:54 -0500

    Get the start and goal points.

--- Robotics/HW4/hw4_team_04.m
+++ Robotics/HW4/hw4_team_04.m
@@ -1,6 +1,14 @@
 function hw4_team_04( R )
-% @param R - the robot (serPort)
+    % @param R - the robot (serPort)
+    
+    [start, goal] = getStartAndGoal();
 
+end
 
+% Get the Start and Goal points
+function [start, goal] = getStartAndGoal()
+    data = importdata('hw4_start_goal.txt');
+    start = data(1,:);
+    goal = data(2,:);
 end
 

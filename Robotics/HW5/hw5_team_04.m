3151435a39bddc732c6e91ffa312251281bfe307
Anthony Dubis <anthonydubis@gmail.com>
2015-12-02 13:19:21 -0500

    Updated with function to use for getting an image.

--- Robotics/HW5/hw5_team_04.m
+++ Robotics/HW5/hw5_team_04.m
@@ -1,10 +1,4 @@
-function hw5_team_04()
-
-% image = imread('http://http://192.168.0.102/img/snapshot.cgi?');
-% imshow(image);
-
-% image = imread('http://192.168.0.102/snapshot.cgi?');
-% imshow(image);
+function hw5_team_04(R)
 
 image = imread('http://192.168.0.102/snapshot.cgi?user=admin&pwd=&resolution=10&rate=0');
 imshow(image);

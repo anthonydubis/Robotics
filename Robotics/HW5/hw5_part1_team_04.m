0cdd1a720e916a9982d4fa4f7a25fd38dc156782
Anthony Dubis <anthonydubis@gmail.com>
2015-12-05 17:42:29 -0500

    Now getting area and centroid of object.

--- Robotics/HW5/hw5_part1_team_04.m
+++ Robotics/HW5/hw5_part1_team_04.m
@@ -1,7 +1,7 @@
 function hw5_part1_team_04(R)
     close all;
     img_path = 'http://192.168.0.102/snapshot.cgi?user=admin&pwd=&resolution=10&rate=0';
-    img_path = 'cone.jpg';
+    img_path = 'stop.jpg';
 
     [t_size, color, t_pos] = get_object_info(img_path);
 
@@ -18,19 +18,32 @@ function [t_size, t_hue, t_pos] = get_object_info(img_path)
     
     % Threshold to find the object
     t_hue = hsv(pt(2),pt(1),1);
-    processed = process_image_for_hue(hsv, t_hue);
-    imshow(processed);
+    BW = process_image_for_hue(hsv, t_hue);
+    imshow(BW);
     
-    t_size = 0;
-    t_pos = 0;
+    object = get_largest_object_stats(BW);
+    
+    t_size = object.FilledArea
+    t_pos = object.Centroid
 end
 
 function processed = process_image_for_hue(hsv, hue)
-    range = 0.03;
+    range = 0.06;
 
     processed = hsv(:,:,1) > hue-range & hsv(:,:,1) < hue+range ...
-        & hsv(:,:,2) > .25 & hsv(:,:,3) > .25;
+        & hsv(:,:,2) > .05 & hsv(:,:,3) > .05;
     
     % Remove noise
     processed = bwareaopen(processed, 20);
+end
+
+function object = get_largest_object_stats(BW)
+    stats = regionprops(BW, 'Centroid', 'FilledArea');
+    
+    object = stats(1);
+    for i=1:length(stats)
+        if stats(i).FilledArea > object.FilledArea
+            object = stats(i);
+        end
+    end
 end

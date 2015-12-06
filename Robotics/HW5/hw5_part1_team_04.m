b908c2888e28f63699a58f8623d4c773096f04ef
Anthony Dubis <anthonydubis@gmail.com>
2015-12-05 20:15:11 -0500

    Implemented code for re-aligning robot

--- Robotics/HW5/hw5_part1_team_04.m
+++ Robotics/HW5/hw5_part1_team_04.m
@@ -4,17 +4,60 @@ function hw5_part1_team_04(R)
     img_path = 'http://192.168.0.102/snapshot.cgi?user=admin&pwd=&resolution=10&rate=0';
     img_path = 'stop.jpg'; % Using test image for now
 
-    [t_area, t_pos, hue] = get_initial_object_info(img_path);
+    [t_area, t_cent, hue, img_sz] = get_initial_object_info(img_path);
     
     while true
-        [n_area, n_pos] = get_object_info(img_path, hue);
+        [n_area, n_cent] = get_object_info(img_path, hue);
+        
+        if should_realign(t_cent, n_cent, img_sz)
+            realign(R, t_cent, n_cent, img_sz);
+        else
+            keep_distance(R, t_area, n_area, img_sz);
+        end
     end
 end
 
+function keep_distance(R, t_area, n_area, img_sz)
+    
+end
+
+% Returns true if the robot needs to realign itself
+% t_cent is the target centroid
+% n_cent is the new centroid
+% img_sz is the image resolution (ex. 320 x 480)
+function realign = should_realign(t_cent, n_cent, img_sz)
+    realign = false;
+    
+    degrees = degrees_misaligned(t_cent, n_cent, img_sz);
+    if abs(degrees) > 5
+        realign = true;
+    end
+end
+
+function realign(R, t_cent, n_cent, img_sz)
+    degrees = degrees_misaligned(t_cent, n_cent, img_sz);
+    turnAngle(R, 0.1, degrees);
+end
+
+% Returns the degrees current centroid is off from the target centroid
+% t_cent is the target centroid
+% n_cent is the new centroid
+% img_sz is the image resolution (ex. 320 x 480)
+% Per the camera, the field of view is 67 degrees
+function degrees = degrees_misaligned(t_cent, n_cent, img_sz)
+    field_of_view = 67;
+    img_width = img_sz(2);
+    pixels_per_degree = img_width / field_of_view;
+    diff = t_cent(2) - n_cent(2);
+    degrees = diff / pixels_per_degree;
+end
+
 % Used to get the initial object information given the user selection
-function [area, pos, hue] = get_initial_object_info(img_path)
+function [area, cent, hue, img_sz] = get_initial_object_info(img_path)
     img = imread(img_path);
     hsv = rgb2hsv(img);
+    sz = size(img);
+    img_sz = sz(:,1:2);
     
     % Get a point on the object from the user
     imshow(img);
@@ -28,11 +71,21 @@ function [area, pos, hue] = get_initial_object_info(img_path)
     object = get_largest_object_stats(BW);
     
     area = object.FilledArea;
-    pos = object.Centroid;
+    cent = object.Centroid;
 end
 
 % Used to get info about the object that matches the given hue
-function [size, pos] = get_object_info(img_path, hue)
+function [area, cent] = get_object_info(img_path, hue)
+    img = imread(img_path);
+    hsv = rgb2hsv(img);
+    
+    BW = process_image_for_hue(hsv, hue);
+    imshow(BW);
+    
+    object = get_largest_object_stats(BW);
+    
+    area = object.FilledArea;
+    cent = object.Centroid;
 end
 
 function processed = process_image_for_hue(hsv, hue)
